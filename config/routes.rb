ba319dc44caf76b8b9e025e944467ad78e17276f
Rob Murray <robmurray17@gmail.com>
2016-02-28 19:34:06 +0000

    Add basic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "main#activity"
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
