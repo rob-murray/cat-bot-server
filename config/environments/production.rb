c6ab7e341f303d2d8c6885b12a6495656b3b0738
Rob Murray <robmurray17@gmail.com>
2016-03-20 18:49:30 +0000

    Allow cable urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://cat-bot.robertomurray.co.uk/cable'
+  config.action_cable.allowed_request_origins = [ 'http://cat-bot.robertomurray.co.uk' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
